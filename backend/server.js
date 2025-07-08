6d8a85e6afbff6fd7fb0276ebf8b2cdcfcc1f4b4
ashkar2023 <muhammedashkar2023@gmail.com>
2025-07-08 13:27:00 +0530

    chore: add timestamp logging on server start

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,18 @@ const server = http.createServer(app);
 
 // Start the server
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
+    console.log(new Date().toLocaleString("en-US", { timeZone: "Asia/kolkata" }))
 });
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err) => {
-  console.error(`Unhandled Rejection: ${err}`);
-  server.close(() => process.exit(1));
+    console.error(`Unhandled Rejection: ${err}`);
+    server.close(() => process.exit(1));
 });
 
 // Handle uncaught exceptions
 process.on('uncaughtException', (err) => {
-  console.error(`Uncaught Exception: ${err}`);
-  server.close(() => process.exit(1));
+    console.error(`Uncaught Exception: ${err}`);
+    server.close(() => process.exit(1));
 });
